958fd78ab39bed575de32a6d12e2561b24aec246
kaplanci <bkaplanc@gmail.com>
2021-01-24 00:12:55 +0300

    20ocak ders sonu

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -19,6 +19,13 @@ namespace GenericsIntro
         {
             T[] tempArray = Items;
             Items = new T[Items.Length +1];
+            for (int i = 0; i <tempArray.Length; i++)
+            {
+                Items[i] = tempArray[i];
+            }
+
+            Items[Items.Length - 1] = item;
+
         }
     }
 }
